c1a699a171e3c0e4c07b37081df43f16e18c539b
zhaoyuqiang <zhaoyuqiang@gmail.com>
2021-04-28 17:35:26 +0800

    第一次提交，作为git测试

---
+++ src/main/java/com/yuqiangz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yuqiangz.git;
+
+/**
+ * @author 赵雨强
+ * @create 2021/4/28 17:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
